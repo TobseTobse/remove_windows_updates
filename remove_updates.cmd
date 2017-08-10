be58fbdc803ea18c68c2d92c8840faecde7391be
Tobse <cthullu@protonmail.com>
2017-08-10 21:45:46 +0200

    Initial commit

---
+++ remove_updates.cmd
@@ -0,0 +1,124 @@
+@echo off
+echo The following list is from https://gist.github.com/xvitaly/eafa75ed2cb79b3bd4e9
+echo Uninstalling KB3075249 (telemetry for Win7/8.1)
+start /w wusa.exe /uninstall /kb:3075249 /quiet /norestart
+echo Uninstalling KB3080149 (telemetry for Win7/8.1)
+start /w wusa.exe /uninstall /kb:3080149 /quiet /norestart
+echo Uninstalling KB3021917 (telemetry for Win7)
+start /w wusa.exe /uninstall /kb:3021917 /quiet /norestart
+echo Uninstalling KB3022345 (telemetry)
+start /w wusa.exe /uninstall /kb:3022345 /quiet /norestart
+echo Uninstalling KB3068708 (telemetry)
+start /w wusa.exe /uninstall /kb:3068708 /quiet /norestart
+echo Uninstalling KB3044374 (Get Windows 10 for Win8.1)
+start /w wusa.exe /uninstall /kb:3044374 /quiet /norestart
+echo Uninstalling KB3035583 (Get Windows 10 for Win7sp1/8.1)
+start /w wusa.exe /uninstall /kb:3035583 /quiet /norestart
+echo Uninstalling KB2990214 (Get Windows 10 for Win7)
+start /w wusa.exe /uninstall /kb:2990214 /quiet /norestart
+echo Uninstalling KB2952664 (Get Windows 10 assistant)
+start /w wusa.exe /uninstall /kb:2952664 /quiet /norestart
+echo Uninstalling KB3075853 (update for "Windows Update" on Win8.1/Server 2012R2)
+start /w wusa.exe /uninstall /kb:3075853 /quiet /norestart
+echo Uninstalling KB3065987 (update for "Windows Update" on Win7/Server 2008R2)
+start /w wusa.exe /uninstall /kb:3065987 /quiet /norestart
+echo Uninstalling KB3050265 (update for "Windows Update" on Win7 - installs a new Group Policy object that enables you to block Win10 upgrades)
+start /w wusa.exe /uninstall /kb:3050265 /quiet /norestart
+echo Uninstalling KB971033  (license validation)
+start /w wusa.exe /uninstall /kb:971033 /quiet /norestart
+echo Uninstalling KB2902907 (description not available)
+start /w wusa.exe /uninstall /kb:2902907 /quiet /norestart
+echo Uninstalling KB2976987 (description not available)
+start /w wusa.exe /uninstall /kb:2976987 /quiet /norestart
+echo Uninstalling KB2976978 (compactibility update for Windows 8.1)
+start /w wusa.exe /uninstall /kb:2976978 /quiet /norestart
+echo Uninstalling KB3102810 (update for "Windows Update")
+start /w wusa.exe /uninstall /kb:3102810 /quiet /norestart
+echo Uninstalling KB3112343 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3112343 /quiet /norestart
+echo Uninstalling KB3135445 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3135445 /quiet /norestart
+echo Uninstalling KB3123862 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3123862 /quiet /norestart
+echo Uninstalling KB3081954 (Telemetry Update for Windows 7)
+start /w wusa.exe /uninstall /kb:3081954 /quiet /norestart
+echo Uninstalling KB3139929 (MS16-023: Security update for Internet Explorer: March 8, 2016 / Get Windows 10 update for MSIE???)
+start /w wusa.exe /uninstall /kb:3139929 /quiet /norestart
+echo Uninstalling KB3138612 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3138612 /quiet /norestart
+echo Uninstalling KB3138615 (Windows Update Client for Windows 8.1)
+start /w wusa.exe /uninstall /kb:3138615 /quiet /norestart
+echo Uninstalling KB3150513 (Compactibility Update (another GWX) for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3150513 /quiet /norestart
+echo Uninstalling KB3133977 (buggy update)
+start /w wusa.exe /uninstall /kb:3133977 /quiet /norestart
+echo Uninstalling KB3139923 (Another GWX for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3139923 /quiet /norestart
+echo Uninstalling KB3173040 (Another GWX for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3173040 /quiet /norestart
+
+echo The following list is from https://forum.tuts4you.com/topic/37497-new-windows-7881-updates-spy-on-you-just-like-windows-10:
+echo Uninstalling KB2882822 (Update adds ITraceRelogger interface support)
+start /w wusa.exe /uninstall /kb:2882822 /quiet /norestart
+echo Uninstalling KB2977759 (Compatibility update for Windows 7 RTM = W10 Diagnostics Compatibility telemetry)
+start /w wusa.exe /uninstall /kb:2977759 /quiet /norestart
+echo Uninstalling KB3012973 (Force Trigger Download and Install of Windows 10)
+start /w wusa.exe /uninstall /kb:3012973 /quiet /norestart
+echo Uninstalling KB3042058 (Microsoft claims its a security update but it contains Winlogon Spying)
+start /w wusa.exe /uninstall /kb:3042058 /quiet /norestart
+echo Uninstalling KB3050267 (Windows 10 upgrade preparation but also adds the option in GPEDIT to disable Windows 10 upgrade altogether)
+start /w wusa.exe /uninstall /kb:3050267 /quiet /norestart
+echo Uninstalling KB3064683 (Windows 10 upgrade for Windows 8)
+start /w wusa.exe /uninstall /kb:3064683 /quiet /norestart
+echo Uninstalling KB3065988 (Windows 10 upgrade for Windows 8)
+start /w wusa.exe /uninstall /kb:3065988 /quiet /norestart
+echo Uninstalling KB3072318 (Windows 10 upgrade preparation for Windows 8)
+start /w wusa.exe /uninstall /kb:3072318 /quiet /norestart
+echo Uninstalling KB3074677 (Windows 10 upgrade preparation for Windows 7)
+start /w wusa.exe /uninstall /kb:3074677 /quiet /norestart
+echo Uninstalling KB3075249 (Update that adds telemetry points to consent.exe in Windows 8.1 and Windows 7)
+start /w wusa.exe /uninstall /kb:3075249 /quiet /norestart
+echo Uninstalling KB3075851 (Windows 10 upgrade for Windows 7)
+start /w wusa.exe /uninstall /kb:3075851 /quiet /norestart
+echo Uninstalling KB3081437 (Windows 10 upgrade preparation)
+start /w wusa.exe /uninstall /kb:3081437 /quiet /norestart
+echo Uninstalling KB3081454 (Windows 10 upgrade preparation)
+start /w wusa.exe /uninstall /kb:3081454 /quiet /norestart
+echo Uninstalling KB3083324 (Windows 10 upgrade preparation for Windows 7)
+start /w wusa.exe /uninstall /kb:3083324 /quiet /norestart
+echo Uninstalling KB3083325 (Windows 10 upgrade preparation for Windows 8)
+start /w wusa.exe /uninstall /kb:3083325 /quiet /norestart
+echo Uninstalling KB3083710 (Update for the Windows Update client with sketchy details for Windows 7. Windows 10 preparation)
+start /w wusa.exe /uninstall /kb:3083710 /quiet /norestart
+echo Uninstalling KB3083711 (Update for the Windows Update client with sketchy details for Windows 8. Windows 10 preparation)
+start /w wusa.exe /uninstall /kb:3083711 /quiet /norestart
+echo Uninstalling KB3090045 (Windows 10 Upgrade Update for Windows 7/8)
+start /w wusa.exe /uninstall /kb:3090045 /quiet /norestart
+echo Uninstalling KB3093513 (Security Update for Windows Shell to Address Remote Code Execution)
+start /w wusa.exe /uninstall /kb:3093513 /quiet /norestart
+echo Uninstalling KB3093983 (Microsoft claims it's a security update but it contains IE spying)
+start /w wusa.exe /uninstall /kb:3093983 /quiet /norestart
+echo Uninstalling KB3102812 (Fixes an issue regarding long wait while searching for Windows Updates but also has Windows 10 upgrade preparation for Windows 8)
+start /w wusa.exe /uninstall /kb:3102812 /quiet /norestart
+echo Uninstalling KB3112336 (Windows 10 Upgrade for Windows 8)
+start /w wusa.exe /uninstall /kb:3112336 /quiet /norestart
+echo Uninstalling KB3118401 (Allows Windows 10 dependant Universal Runtime apps to run on earlier versions of Windows)
+start /w wusa.exe /uninstall /kb:3118401 /quiet /norestart
+
+echo The following Updates have been suggested by User wincat71 on https://gist.github.com/xvitaly/eafa75ed2cb79b3bd4e9
+echo Uninstalling KB3045999 (description not available)
+start /w wusa.exe /uninstall /kb:3045999 /quiet /norestart
+echo Uninstalling KB2919355 (description not available)
+start /w wusa.exe /uninstall /kb:2919355 /quiet /norestart
+echo Uninstalling KB3015249 (description not available)
+start /w wusa.exe /uninstall /kb:3015249 /quiet /norestart
+echo Uninstalling KB2922324 (description not available)
+start /w wusa.exe /uninstall /kb:2922324 /quiet /norestart
+echo Uninstalling KB3088195 (description not available)
+start /w wusa.exe /uninstall /kb:3088195 /quiet /norestart
+echo Uninstalling KB2505438 (description not available)
+start /w wusa.exe /uninstall /kb:2505438 /quiet /norestart
+echo Uninstalling KB2670838 (description not available)
+start /w wusa.exe /uninstall /kb:2670838 /quiet /norestart
+echo Uninstalling KB3072630 (description not available)
+start /w wusa.exe /uninstall /kb:3072630 /quiet /norestart
